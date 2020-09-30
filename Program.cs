46ed9cdf4beb442e14162726763dd084ef57bd32
Randy Topken <rtopken@hotmail.com>
2020-09-30 16:51:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strDownloads = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            strDownloads += "\\Downloads";
+
+            DirectoryInfo dInfo = new DirectoryInfo(strDownloads);
+
+            FileInfo[] dlFiles = dInfo.GetFiles();
+
+        }
+    }
+}
